b98698dacd18f0f5132e4bb65cb0fafb15a2ae18
pratik-p-crest <pratik.p@crestskillserve.com>
2026-01-20 10:53:59 +0530

    PostgreSQL

--- 03 - PostgreSQL/04 - Querying Data/01 - SELECT , ORDER BY & DISTINCT.sql
+++ 03 - PostgreSQL/04 - Querying Data/01 - SELECT , ORDER BY & DISTINCT.sql
@@ -76,4 +76,88 @@ SELECT
 	*
 FROM movies
 ORDER BY
-	release_date D; 
+	release_date DESC; 
+
+
+-- sort based on multiple column
+SELECT *
+FROM movies
+ORDER BY
+	release_date DESC,
+	movie_name ASC;
+
+
+-- here first movie name will be ordered and the by release date
+SELECT *
+FROM movies
+ORDER BY
+	movie_name ASC,
+	release_date DESC;
+
+
+-- order by with alias coloumn name
+SELECT 
+	first_name,
+	last_name AS surname
+FROM actors
+ORDER BY last_name ;
+
+
+SELECT 
+	first_name,
+	last_name AS surname
+FROM actors
+ORDER BY surname DESC;  -- you can use alias name as well
+
+
+-- calculate length of the actor name with LENGTH function
+SELECT
+	first_name,
+	LENGTH(first_name) 
+FROM actors;
+
+SELECT
+	first_name,
+	LENGTH(first_name) as len
+FROM actors
+ORDER BY
+	len DESC;
+
+
+SELECT 5 * 20
+
+
+-- can use coloumn number to do order by
+SELECT 
+	first_name,
+	last_name
+FROM actors
+ORDER BY
+	1 ASC,
+	2 DESC;
+
+-- get unique values
+SELECT
+	DISTINCT movie_lang
+FROM movies
+ORDER BY movie_lang;
+
+
+-- get unique director id
+SELECT
+	DISTINCT director_id
+FROM movies
+ORDER BY 1;
+
+
+-- multiple distinct values
+SELECT
+	DISTINCT movie_lang, director_id  -- here it works on both coloumn's combination
+FROM movies
+ORDER BY 1;
+
+
+-- all unique records from table
+SELECT 
+	DISTINCT *
+FROM movies;
